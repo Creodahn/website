2521e39a1a2a7faa2b10dbde2fef56866c67d060
Justin Drew <jdrew@sca-corp.com>
2016-07-06 22:31:43 -0400

    add blog background image; create make-safe helper for making attributes that evaluate to css acceptable; better background image transitioning

--- app/main/work-experience/route.js
+++ app/main/work-experience/route.js
@@ -1,14 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  beforeModel: function() {
+    this.controllerFor('main').set('style', '');
+  },
   model: function() {
     return this.modelFor('main').get('workExperiences').then((exp) => {
       return exp;
     });
   },
-  actions: {
-    didTransition: function() {
-      this.controllerFor('main').set('style', 'background-image: url(assets/imgs/work.jpg)');
-    }
+  afterModel: function() {
+    this.controllerFor('main').set('style', 'background-image: url(assets/imgs/work.jpg)');
   }
 });
---
+++ tests/unit/helpers/make-safe-test.js
@@ -0,0 +1,10 @@
+import { makeSafe } from 'personal-website/helpers/make-safe';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | make safe');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = makeSafe([42]);
+  assert.ok(result);
+});
